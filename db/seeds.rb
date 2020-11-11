67466bcccf115610e144727a76b086a506a2fb4d
Sean OReilly <soreilly424@gmail.com>
2020-11-11 18:54:13 -0500

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+        # t.string :name
+#       t.string :address
+#       t.text :description
+#       t.integer :price_per_night
+#       t.integer :number_of_guests
+
+4.times do 
+  Flat.create!(
+    name: Faker::Book.genre + ' flat',
+    address: Faker::Address.city,
+    description: Faker::JapaneseMedia::StudioGhibli.quote,
+    price_per_night: Faker::Number.between(from: 40, to: 1000),
+    number_of_guests: Faker::Number.between(from: 1, to: 10)
+  )
+end
